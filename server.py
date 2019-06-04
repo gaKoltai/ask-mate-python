2c7d96f3948c2a67ec92bffbbc15e0fd34e9557a
Mate Iszlai <iszimate@gmail.com>
2019-06-04 11:55:50 +0200

    Merge branch 'master' of github.com:gaKoltai/ask-mate-python
    # Conflicts:
    #	server.py

--- server.py
+++ server.py
@@ -5,12 +5,17 @@ import data_manager
 app = Flask(__name__)
 
 
-@app.route('/')
-def route_list():
-    user_questions = connection.get_info_from_file(connection.QUESTION_FILE)
-    data_manager.add_line_breaks_to_data(user_questions)
-    data_manager.get_post_time(user_questions)
-    return render_template('list.html', user_questions =user_questions)
+@app.route('/', methods = ['POST', 'GET'])
+def route_questions(vote = None, id=None):
+    if request.method == 'GET':
+        user_questions = connection.get_info_from_file(connection.QUESTION_FILE)
+        data_manager.add_line_breaks_to_data(user_questions)
+        data_manager.get_post_time(user_questions)
+        return render_template('list.html', user_questions =user_questions)
+    if request.method == 'POST':
+        if vote:
+            data_manager.vote(vote, id)
+            return redirect(url_for('route_questions'))
 
 
 @app.route('/question/<int:question_id>')
@@ -19,6 +24,12 @@ def route_question_with_answer(question_id=None):
         question = data_manager.get_question_by_id(question_id)
         answers = data_manager.get_answers_by_question_id(question_id)
     return render_template('question_with_answers.html', question=question, answers=answers)
+
+
+@app.route('/question/<int:question_id>/<vote>')
+def route_vote(question_id=None, vote = None):
+    data_manager.vote(question_id, vote)
+    return redirect(render_template(url_for('route_questions')))
 
 
 @app.route('/question/<int:question_id>/new-answer', methods=['GET', 'POST'])
