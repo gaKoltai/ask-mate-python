7d4b8ad733afad898a2d87aa5658c7f4f4cbd1d0
Mate Iszlai <iszimate@gmail.com>
2019-06-04 10:39:29 +0200

    Added get_question_by_id, get_answer_by_question_id functions

--- data_manager.py
+++ data_manager.py
@@ -20,3 +20,21 @@ def add_line_breaks_to_data(user_data):
 
     return user_data
 
+
+def get_question_by_id(question_id):
+    searched_question = {}
+    questions = connection.get_info_from_file(connection.QUESTION_FILE)
+    for question in questions:
+        if question['id'] == str(question_id):
+            for item, value in question.items():
+                searched_question[item] = value
+    return searched_question
+
+
+def get_answers_by_question_id(question_id):
+    searched_answers = []
+    answers = connection.get_info_from_file(connection.ANSWER_FILE)
+    for answer in answers:
+        if answer['question_id'] == str(question_id):
+            searched_answers.append(answer)
+    return searched_answers
