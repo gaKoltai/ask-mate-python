cf6079cd1204f9fb854e3cace3207f17d0e5ddc5
Mate Iszlai <iszimate@gmail.com>
2019-06-04 10:41:52 +0200

    Refactoring route_question_with_answer

--- server.py
+++ server.py
@@ -15,22 +15,10 @@ def route_list():
 
 @app.route('/question/<int:question_id>')
 def route_question_with_answer(question_id=None):
-    searched_question = {}
-    searched_answers = []
     if question_id is not None:
-        questions = connection.get_info_from_file(connection.QUESTION_FILE)
-
-        for question in questions:
-            if question['id'] == str(question_id):
-                for item, value in question.items():
-                    searched_question[item] = value
-        answers = connection.get_info_from_file(connection.ANSWER_FILE)
-        for answer in answers:
-            if answer['question_id'] == str(question_id):
-                searched_answers.append(answer)
-        #question = data_manager.get_question_by_id(question_id)
-        #answers = data_manager.get_answers_by_question_id(question_id)
-    return render_template('question_with_answers.html', question=searched_question, answers=searched_answers)
+        question = data_manager.get_question_by_id(question_id)
+        answers = data_manager.get_answers_by_question_id(question_id)
+    return render_template('question_with_answers.html', question=question, answers=answers)
 
 
 if __name__ == '__main__':
