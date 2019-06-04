9a2b2cb52dee4d31e024ca76304e49b57a209817
Gergely Koltai <kgalmos@gmail.com>
2019-06-04 13:57:16 +0200

    Fixed get_question/answer by id to display post time correctly

--- data_manager.py
+++ data_manager.py
@@ -41,6 +41,7 @@ def add_line_breaks_to_data(user_data):
 def get_question_by_id(question_id):
     searched_question = {}
     questions = connection.get_info_from_file(connection.QUESTION_FILE)
+    get_post_time(questions)
     for question in questions:
         if question['id'] == str(question_id):
             for item, value in question.items():
@@ -51,6 +52,7 @@ def get_question_by_id(question_id):
 def get_answers_by_question_id(question_id):
     searched_answers = []
     answers = connection.get_info_from_file(connection.ANSWER_FILE)
+    get_post_time(answers)
     for answer in answers:
         if answer['question_id'] == str(question_id):
             searched_answers.append(answer)
